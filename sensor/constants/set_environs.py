283cc8bb713ca645284ee05cba66ab36930ae2cf
Lakshay03Feb <lakshaymalhotra3@gmail.com>
2022-10-31 23:57:41 +0530

    changes in gitignore

--- sensor/constants/set_environs.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ["mongo_db_url"] = "mongodb+srv://lakshay03:limit123456@cluster0.ryyuevc.mongodb.net/?retryWrites=true&w=majority"
-mongo_db_url = os.environ["mongo_db_url"]
-
-
